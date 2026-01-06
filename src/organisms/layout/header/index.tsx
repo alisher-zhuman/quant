c099948b9fefbabe932a5b83ba27dddb4e59d851
Alisher <zumanovaliser814@gmail.com>
2026-01-06 14:03:03 +0600

    header width fix

--- src/organisms/layout/header/index.tsx
+++ src/organisms/layout/header/index.tsx
@@ -19,67 +19,69 @@ export const Header = () => {
 
   return (
     <>
-      <header className="max-w-[1600px] w-full m-auto py-4 pl-4.5 lgx:pl-9.5 pr-5 lgx:pr-10 flex items-center justify-between sticky top-0 bg-white z-50">
-        <Link href="/">
-          <Image
-            src="/icons/logo.svg"
-            alt="Логотип"
-            className="h-10 w-10 md:w-16 md:h-16"
-            loading="eager"
-            width={64}
-            height={64}
-          />
-        </Link>
-
-        <div className="flex items-center gap-1 sm:gap-4 lg:gap-3">
-          <Navbar />
-
-          <Button className="text-sm sm:text-base" onClick={toggleModal}>
-            {t("Получить консультацию")}
-          </Button>
-
-          <Image
-            onClick={() => setIsMenuOpen(true)}
-            className="lg:hidden cursor-pointer"
-            src="/icons/burger.svg"
-            alt="Меню"
-            width={24}
-            height={24}
-          />
-        </div>
-
-        <div
-          className={cn(
-            "fixed inset-0 bg-black/50 z-40 transition-opacity duration-300",
-            isMenuOpen
-              ? "opacity-100 pointer-events-auto"
-              : "opacity-0 pointer-events-none"
-          )}
-          onClick={() => setIsMenuOpen(false)}
-        />
-
-        <div
-          className={cn(
-            "fixed top-0 right-0 w-4/5 max-w-sm h-full bg-white z-50 flex flex-col p-2 transform transition-transform duration-300 ease-in-out",
-            isMenuOpen ? "translate-x-0" : "translate-x-full"
-          )}
-        >
-          <div className="flex justify-end">
+      <header className="w-full sticky top-0 bg-white z-50">
+        <div className="max-w-[1600px] m-auto py-4 pl-4.5 lgx:pl-9.5 pr-5 lgx:pr-10 flex items-center justify-between">
+          <Link href="/">
             <Image
-              onClick={() => setIsMenuOpen(false)}
-              className="cursor-pointer"
-              src="/icons/close.svg"
-              alt="Закрыть"
+              src="/icons/logo.svg"
+              alt="Логотип"
+              className="h-10 w-10 md:w-16 md:h-16"
+              loading="eager"
+              width={64}
+              height={64}
+            />
+          </Link>
+
+          <div className="flex items-center gap-1 sm:gap-4 lg:gap-3">
+            <Navbar />
+
+            <Button className="text-sm sm:text-base" onClick={toggleModal}>
+              {t("Получить консультацию")}
+            </Button>
+
+            <Image
+              onClick={() => setIsMenuOpen(true)}
+              className="lg:hidden cursor-pointer"
+              src="/icons/burger.svg"
+              alt="Меню"
               width={24}
               height={24}
             />
           </div>
 
-          <Button className="mt-2" onClick={toggleModal}>
-            {t("Получить консультацию")}
-          </Button>
+          <div
+            className={cn(
+              "fixed inset-0 bg-black/50 z-40 transition-opacity duration-300",
+              isMenuOpen
+                ? "opacity-100 pointer-events-auto"
+                : "opacity-0 pointer-events-none"
+            )}
+            onClick={() => setIsMenuOpen(false)}
+          />
 
-          <Navbar mobile closeMenu={() => setIsMenuOpen(false)} />
+          <div
+            className={cn(
+              "fixed top-0 right-0 w-4/5 max-w-sm h-full bg-white z-50 flex flex-col p-2 transform transition-transform duration-300 ease-in-out",
+              isMenuOpen ? "translate-x-0" : "translate-x-full"
+            )}
+          >
+            <div className="flex justify-end">
+              <Image
+                onClick={() => setIsMenuOpen(false)}
+                className="cursor-pointer"
+                src="/icons/close.svg"
+                alt="Закрыть"
+                width={24}
+                height={24}
+              />
+            </div>
+
+            <Button className="mt-2" onClick={toggleModal}>
+              {t("Получить консультацию")}
+            </Button>
+
+            <Navbar mobile closeMenu={() => setIsMenuOpen(false)} />
+          </div>
         </div>
       </header>
 
